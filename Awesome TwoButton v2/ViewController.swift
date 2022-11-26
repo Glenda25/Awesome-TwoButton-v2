48dd53a6788628ba719fc0368a62c66101e45c96
Glenda Mullan <glenda.mullan@gmail.com>
2022-11-26 14:35:54 +1100

    adding viewController scripts

--- Awesome TwoButton v2/ViewController.swift
+++ Awesome TwoButton v2/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var placeHolder: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Add1(_ sender: UIButton) {
+    }
+    
+    @IBAction func Add2(_ sender: UIButton) {
+    }
 }
 
