843144340589360f1c1c51dc6d1ce84fcd6b00c2
Glenda Mullan <glenda.mullan@gmail.com>
2022-11-26 16:38:34 +1100

    completed

--- Awesome TwoButton v2/ViewController.swift
+++ Awesome TwoButton v2/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var total = 0
 
     @IBOutlet weak var placeHolder: UILabel!
     
@@ -17,9 +19,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func Add1(_ sender: UIButton) {
+        total = total + 1
+        placeHolder.text = String(total)
+        
+        if placeHolder.textColor == .red {
+            placeHolder.textColor = .green
+        }else {
+            placeHolder.textColor = .blue
+        }
     }
     
     @IBAction func Add2(_ sender: UIButton) {
+        total = total + 2
+        placeHolder.text = String(total)
+        placeHolder.textColor = .red
+        
     }
 }
 
